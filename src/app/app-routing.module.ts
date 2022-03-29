9643fb454409687a79683b35a4a59a6d0b14f006
David Avikasis <deddy@deddyxyz16.local>
2022-03-29 22:12:19 +0300

    Added Catalog Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CatalogPageComponent } from './components/catalog-page/catalog-page.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: HomePageComponent,
+  },
+  { path: 'catalog', component: CatalogPageComponent },
+  { path: 'contact', redirectTo: '/#contact' },
+  // { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, {
+      scrollPositionRestoration: 'enabled',
+      anchorScrolling: 'enabled',
+    }),
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/components/catalog-page/catalog-page.component.ts
+++ src/app/components/catalog-page/catalog-page.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-catalog-page',
   templateUrl: './catalog-page.component.html',
-  styleUrls: ['./catalog-page.component.scss']
+  styleUrls: ['./catalog-page.component.scss'],
 })
 export class CatalogPageComponent implements OnInit {
+  constructor(private productService: ProductService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  get products() {
+    return this.productService.getProducts();
   }
-
 }
