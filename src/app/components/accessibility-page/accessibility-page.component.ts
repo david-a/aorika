e240d1b0fc839909db26f1fdf8de74d6e57d3aaa
David Avikasis <deddy@deddyxyz16.local>
2022-10-09 18:06:18 +0300

    Added seo stuff

--- src/app/components/accessibility-page/accessibility-page.component.ts
+++ src/app/components/accessibility-page/accessibility-page.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-accessibility-page',
   templateUrl: './accessibility-page.component.html',
-  styleUrls: ['./accessibility-page.component.scss']
+  styleUrls: ['./accessibility-page.component.scss'],
 })
 export class AccessibilityPageComponent implements OnInit {
-
-  constructor() { }
+  title = 'אאוריקה - הצהרת נגישות';
+  constructor(private titleService: Title, private metaService: Meta) {}
 
   ngOnInit(): void {
+    this.titleService.setTitle(this.title);
+    this.metaService.updateTag({
+      property: 'og:url',
+      content: environment.baseUrl + 'accessibility',
+    });
   }
-
 }
